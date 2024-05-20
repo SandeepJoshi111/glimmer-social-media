190b4b32deec15211617f5f44c370694c85ec552
Sandeep Joshi <joshisandip111@gmail.com>
2024-05-20 14:19:09 +0545

    profile page with follow and unfollow function

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -5,9 +5,28 @@ import {
   Flex,
   Text,
   VStack,
+  useDisclosure,
 } from "@chakra-ui/react";
+import useUserProfileStore from "../../store/userProfileStore";
+import useAuthStore from "../../store/authStore";
+import EditProfile from "./EditProfile";
+import useFollowUser from "../../hooks/useFollowUser";
 
 function ProfileHeader() {
+  const { userProfile } = useUserProfileStore();
+  const authUser = useAuthStore((state) => state.user);
+  const visitingOwnProfileAndAuth =
+    authUser && authUser.username === userProfile.username;
+  const visitingAnotherProfileAndAuth =
+    authUser && authUser.username !== userProfile.username;
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  // follow Unfollow
+  const { isFollowing, handleFollowUser, isUpdating } = useFollowUser(
+    userProfile?.uid
+  );
+
   return (
     <Flex
       gap={{ base: 4, sm: 10 }}
@@ -20,11 +39,7 @@ function ProfileHeader() {
         alignSelf={"flex-start"}
         mx={"auto"}
       >
-        <Avatar
-          name="As a Programmer"
-          src="/profilepic.png"
-          alt="asaprogrammer"
-        />
+        <Avatar src={userProfile.profilePicURL} alt={userProfile.username} />
       </AvatarGroup>
 
       <VStack alignItems={"start"} gap={2} mx={"auto"} flex={1}>
@@ -35,45 +50,66 @@ function ProfileHeader() {
           alignItems={"center"}
           w={"full"}
         >
-          <Text fontSize={{ base: "sm", md: "lg" }}>asaprogrammer</Text>
-          <Flex gap={4} alignItems={"center"} justifyContent={"center"}>
-            <Button
-              bg={"white"}
-              color={"black"}
-              _hover={{ bg: "whiteAlpha.800" }}
-              size={{ base: "xs", md: "sm" }}
-            >
-              Edit Profile
-            </Button>
-          </Flex>
+          <Text fontSize={{ base: "sm", md: "lg" }}>
+            {userProfile.username}
+          </Text>
+          {visitingOwnProfileAndAuth && (
+            <Flex gap={4} alignItems={"center"} justifyContent={"center"}>
+              <Button
+                bg={"white"}
+                color={"black"}
+                _hover={{ bg: "whiteAlpha.800" }}
+                size={{ base: "xs", md: "sm" }}
+                onClick={onOpen}
+              >
+                Edit Profile
+              </Button>
+            </Flex>
+          )}
+          {visitingAnotherProfileAndAuth && (
+            <Flex gap={4} alignItems={"center"} justifyContent={"center"}>
+              <Button
+                bg={"blue.500"}
+                color={"white"}
+                _hover={{ bg: "blue.600" }}
+                size={{ base: "xs", md: "sm" }}
+                onClick={handleFollowUser}
+                isLoading={isUpdating}
+              >
+                {isFollowing ? "Unfollow" : "Follow"}
+              </Button>
+            </Flex>
+          )}
         </Flex>
         <Flex alignItems={"center"} gap={{ base: 2, sm: 4 }}>
           <Text fontSize={{ base: "xs", md: "sm" }}>
             <Text as="span" fontWeight={"bold"} mr={1}>
-              4
+              {userProfile.posts.length}
             </Text>
             Posts
           </Text>
           <Text fontSize={{ base: "xs", md: "sm" }}>
             <Text as="span" fontWeight={"bold"} mr={1}>
-              123
+              {userProfile.followers.length}
             </Text>
             Followers
           </Text>
           <Text fontSize={{ base: "xs", md: "sm" }}>
             <Text as="span" fontWeight={"bold"} mr={1}>
-              190
+              {userProfile.following.length}
             </Text>
             Following
           </Text>
         </Flex>
         <Flex alignItems={"center"} gap={4}>
           <Text fontSize={"sm"} fontWeight={"bold"}>
-            As a Programmer
+            {userProfile.fullName}
           </Text>
         </Flex>
-        <Text fontSize={"sm"}>Tutorial are important for a Programmer</Text>
+        <Text fontSize={"sm"}>{userProfile.bio}</Text>
       </VStack>
+
+      {isOpen && <EditProfile isOpen={isOpen} onClose={onClose} />}
     </Flex>
   );
 }
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,41 +1,12 @@
-import { Avatar, Box, Button, Flex, Link, Tooltip } from "@chakra-ui/react";
+import { Box, Button, Flex, Link, Tooltip } from "@chakra-ui/react";
 import { Link as RouterLink } from "react-router-dom";
-import {
-  CreatePostLogo,
-  InstagramLogo,
-  InstagramMobileLogo,
-  NotificationsLogo,
-  SearchLogo,
-} from "../../assets/constants";
-import { AiFillHome } from "react-icons/ai";
+import { InstagramLogo, InstagramMobileLogo } from "../../assets/constants";
+
 import { BiLogOut } from "react-icons/bi";
 import useLogout from "../../hooks/useLogout";
-function Sidebar() {
-  const sidebarItems = [
-    {
-      icon: <AiFillHome size={25} />,
-      text: "Home",
-      link: "/",
-    },
-    {
-      icon: <SearchLogo size={25} />,
-      text: "Search",
-    },
-    {
-      icon: <NotificationsLogo size={25} />,
-      text: "Notification",
-    },
-    {
-      icon: <CreatePostLogo size={25} />,
-      text: "Create",
-    },
-    {
-      icon: <Avatar size={"sm"} name="Burak orkmez" src="/profilepic.png" />,
-      text: "Profile",
-      link: "/asaprogrammer",
-    },
-  ];
+import SidebarItems from "./SidebarItems";
 
+const Sidebar = () => {
   const { handleLogout, isLoggingOut } = useLogout();
   return (
     <Box
@@ -48,61 +19,35 @@ function Sidebar() {
       left={0}
       px={{ base: 2, md: 4 }}
     >
-      <Flex direction={"column"} gap={10} w={"full"} height={"full"}>
+      <Flex direction={"column"} gap={10} w="full" height={"full"}>
         <Link
-          as={RouterLink}
           to={"/"}
+          as={RouterLink}
           pl={2}
-          display={{ base: "none", md: "block " }}
-          cursor={"pointer"}
+          display={{ base: "none", md: "block" }}
+          cursor="pointer"
         >
           <InstagramLogo />
         </Link>
         <Link
-          p={2}
-          as={RouterLink}
           to={"/"}
+          as={RouterLink}
+          p={2}
           display={{ base: "block", md: "none" }}
           borderRadius={6}
-          cursor={"pointer"}
+          _hover={{
+            bg: "whiteAlpha.200",
+          }}
           w={10}
-          _hover={{ bg: "whiteAlpha.200" }}
+          cursor="pointer"
         >
           <InstagramMobileLogo />
         </Link>
-
-        {/* Nav Links */}
         <Flex direction={"column"} gap={5} cursor={"pointer"}>
-          {sidebarItems.map((item, index) => (
-            <Tooltip
-              key={index}
-              hasArrow
-              label={item.text}
-              placement="right"
-              ml={1}
-              openDelay={500}
-              display={{ base: "block", md: "none" }}
-            >
-              <Link
-                display={"flex"}
-                to={item.link || null}
-                as={RouterLink}
-                alignItems={"center"}
-                gap={4}
-                _hover={{ bg: "whiteAlpha.400" }}
-                borderRadius={6}
-                p={2}
-                w={{ base: "10", md: "full" }}
-                justifyContent={{ base: "center", md: "flex-start" }}
-              >
-                {item.icon}
-                <Box display={{ base: "none", md: "block" }}>{item.text}</Box>
-              </Link>
-            </Tooltip>
-          ))}
+          <SidebarItems />
         </Flex>
 
-        {/* Logout */}
+        {/* LOGOUT */}
         <Tooltip
           hasArrow
           label={"Logout"}
@@ -118,9 +63,9 @@ function Sidebar() {
             _hover={{ bg: "whiteAlpha.400" }}
             borderRadius={6}
             p={2}
-            w={{ base: "10", md: "full" }}
-            justifyContent={{ base: "center", md: "flex-start" }}
+            w={{ base: 10, md: "full" }}
             mt={"auto"}
+            justifyContent={{ base: "center", md: "flex-start" }}
           >
             <BiLogOut size={25} />
             <Button
@@ -129,13 +74,13 @@ function Sidebar() {
               _hover={{ bg: "transparent" }}
               isLoading={isLoggingOut}
             >
-              Log out
+              Logout
             </Button>
           </Flex>
         </Tooltip>
       </Flex>
     </Box>
   );
-}
+};
 
 export default Sidebar;
