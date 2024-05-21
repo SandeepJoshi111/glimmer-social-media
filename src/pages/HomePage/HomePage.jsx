1f837df3efa82b3d0dcfb13320fcc41820d0bd39
Sandeep Joshi <joshisandip111@gmail.com>
2024-05-21 14:04:07 +0545

    ui design fix

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -9,12 +9,7 @@ function HomePage() {
         <Box flex={2} py={10}>
           <FeedPosts />
         </Box>
-        <Box
-          flex={3}
-          mr={20}
-          display={{ base: "none", md: "block" }}
-          maxW={"300px"}
-        >
+        <Box flex={3} display={{ base: "none", lg: "block" }} maxW={"300px"}>
           <SuggestedUsers />
         </Box>
       </Flex>
